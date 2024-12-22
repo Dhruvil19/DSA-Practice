f198a477a363904a9ef2b1e6f73c2bd9e762308d
Meniya Dhruvil <meniyadhruvil@gmail.com>
2024-12-22 08:08:47 +0530

    Initial commit

---
+++ pettarn2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int row=5;
+    int column=5;
+    for (int i=0 ;i<row;i++){
+    for (int j=0; j<column;j++){
+        cout << j ;
+        cout << " ";
+            }
+            cout<< endl;
+            
+    }
+
+
+}

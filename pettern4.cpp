c2cc12d75f11863687bf33e772c522f99a7e0ed8
Meniya Dhruvil <meniyadhruvil@gmail.com>
2024-12-22 08:35:16 +0530

    code

---
+++ pettern4.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // 1    2   3
+    //1    2   3
+   // 1    2   3
+   for( int i=0;i<=8;i++){
+          for(int j=1;j<=8;j++){
+            cout<< j;
+
+          }
+       cout<<endl;
+   }
+    return 0;
+
+  
+
+}

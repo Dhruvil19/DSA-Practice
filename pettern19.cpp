3925c1b1d15c47a9f0b59e2a0a9fe3e2c20e1aff
Meniya Dhruvil <meniyadhruvil@gmail.com>
2024-12-25 09:26:20 +0530

    1-20 complated pettern

---
+++ pettern19.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    for(int i=1;i<=3;i++){
+        for (int j=1;j<=3;j++){
+        char ch=i+j+'A'-1-1;
+            cout<<ch<<" ";
+            
+        }
+        cout<<endl;
+        
+    }
+}   
